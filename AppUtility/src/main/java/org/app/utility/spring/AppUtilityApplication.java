cadb25760cee243ab391fea842fceb036d3a532e
Soumya Ranjan Panda <psoumyaranjan39@gmail.com>
2025-10-08 14:51:10 +0530

    initialize Spring Boot application with Maven wrapper and basic configuration

---
+++ AppUtility/src/main/java/org/app/utility/spring/AppUtilityApplication.java
@@ -0,0 +1,13 @@
+package org.app.utility.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppUtilityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppUtilityApplication.class, args);
+	}
+
+}
---
+++ AppUtility/src/test/java/org/app/utility/spring/AppUtilityApplicationTests.java
@@ -0,0 +1,13 @@
+package org.app.utility.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppUtilityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
